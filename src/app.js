1d9d8c3c475ff6ebde57e4752b7f15e4fae3ccd2
Sergey Mysov <s.mysov@list.ru>
2021-02-26 18:30:56 +0400

    added function fullname

--- src/app.js
+++ src/app.js
@@ -1,11 +1,5 @@
-const sum = (a, b) => a + b;
+const name = 'Max';
 
-sum(2, 3);
+const fullname = (name, surname) => `${name} ${surname}`;
 
-const multiple = (num1, num2) => num1 + num2;
-
-multiple(2, 3);
-
-const div = (num1, num2) => num1 + num2;
-
-div(9, 3);
+fullname(name, 'Ivanov');
